fb2e8f638a86c3f8025377db1bf3b258d1ceafc0
Marko Kostic <archmarko92@gmail.com>
2025-04-22 18:38:09 +0200

    Reorganize so that Generator class is called with parsed arguments

--- Program.cs
+++ Program.cs
@@ -7,24 +7,7 @@ Console.WriteLine("Hello, World!");
 IEnumerable<string> files;
 
 ParserResult<Options> parserResult = Parser.Default.ParseArguments<Options>(args);
-parserResult.WithParsed(
-    o =>
-    {
-        if (!Directory.Exists(o.SearchDirectory))
-        {
-            throw new Exception("Search directory does not exist!");
-        }
-
-        IEnumerable<string> hFiles = Directory.EnumerateFiles(o.SearchDirectory, "*.h", SearchOption.AllDirectories);
-        IEnumerable<string> hppFiles = Directory.EnumerateFiles(o.SearchDirectory, "*.hpp", SearchOption.AllDirectories);
-        files = hFiles.Concat(hppFiles);
-
-        foreach (string file in files)
-        {
-            Console.WriteLine(file);
-        }
-    }
-);
+parserResult.WithParsed(Generator.RunProgram);
 
 namespace Obsidian
 {
@@ -32,5 +15,34 @@ namespace Obsidian
     {
         [Option("search-path", Required = true, HelpText = "Directory to recuresively search for C++ include files.")]
         public string SearchDirectory { get; set; } = string.Empty;
+    }
+
+    public class Generator
+    {
+        public static void RunProgram(Options options)
+        {
+            Generator generator = new(options.SearchDirectory);
+            IEnumerable<string> headerFiles = generator.GetHeaderFiles();
+        }
+
+        private string _searchDirectory = string.Empty;
+
+        public Generator(string searchDirectory)
+        {
+            _searchDirectory = searchDirectory;
+            if (!Directory.Exists(_searchDirectory))
+            {
+                throw new Exception("Search directory does not exist!");
+            }
+
+            
+        }
+
+        public IEnumerable<string> GetHeaderFiles()
+        {
+            IEnumerable<string> hFiles = Directory.EnumerateFiles(_searchDirectory, "*.h", SearchOption.AllDirectories);
+            IEnumerable<string> hppFiles = Directory.EnumerateFiles(_searchDirectory, "*.hpp", SearchOption.AllDirectories);
+            return hFiles.Concat(hppFiles);
+        }
     }
 }
