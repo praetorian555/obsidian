23a6492e4e18105bbd1ea0beeb809ecbad836bc8
Marko Kostic <archmarko92@gmail.com>
2025-04-25 20:13:14 +0200

    Fix compare

--- test-cpp/src/main.cpp
+++ test-cpp/src/main.cpp
@@ -26,7 +26,7 @@ TEST_CASE("Enum to name", "[refl][enum]")
     std::string full_enum_name = Obs::Enum<Fruit>::GetFullEnumName();
     REQUIRE(full_enum_name == "FirstNamespace::SecondNamespace::Fruit");
 
-    REQUIRE(Obs::Enum<Fruit>::GetName(Fruit::Apple) == "Apple");
-    REQUIRE(Obs::Enum<Fruit>::GetName(Fruit::Orange) == "Orange");
-    REQUIRE(Obs::Enum<Fruit>::GetName(Fruit::Banana) == "Banana");
+    REQUIRE(strcmp(Obs::Enum<Fruit>::GetName(Fruit::Apple), "Apple") == 0);
+    REQUIRE(strcmp(Obs::Enum<Fruit>::GetName(Fruit::Orange), "Orange") == 0);
+    REQUIRE(strcmp(Obs::Enum<Fruit>::GetName(Fruit::Banana), "Banana") == 0);
 }
