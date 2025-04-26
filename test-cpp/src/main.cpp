6c13b67cc135809525d169124d19153bc33c6186
Marko Kostic <archmarko92@gmail.com>
2025-04-26 10:36:37 +0200

    Add tests for enum reflection

--- test-cpp/src/main.cpp
+++ test-cpp/src/main.cpp
@@ -26,7 +26,21 @@ TEST_CASE("Enum to name", "[refl][enum]")
     std::string full_enum_name = Obs::Enum<Fruit>::GetFullEnumName();
     REQUIRE(full_enum_name == "FirstNamespace::SecondNamespace::Fruit");
 
-    REQUIRE(strcmp(Obs::Enum<Fruit>::GetName(Fruit::Apple), "Apple") == 0);
-    REQUIRE(strcmp(Obs::Enum<Fruit>::GetName(Fruit::Orange), "Orange") == 0);
-    REQUIRE(strcmp(Obs::Enum<Fruit>::GetName(Fruit::Banana), "Banana") == 0);
+    REQUIRE(Obs::Enum<Fruit>::GetUnderlyingValue(Obs::Enum<Fruit>::k_end) == 8);
+
+    REQUIRE(strcmp(Obs::Enum<Fruit>::GetValueName(Fruit::Apple), "Apple") == 0);
+    REQUIRE(strcmp(Obs::Enum<Fruit>::GetValueName(Fruit::Orange), "Orange") == 0);
+    REQUIRE(strcmp(Obs::Enum<Fruit>::GetValueName(Fruit::Banana), "Banana") == 0);
+
+    REQUIRE(Obs::Enum<Fruit>::GetValue("Apple") == Fruit::Apple);
+    REQUIRE(Obs::Enum<Fruit>::GetValue("Orange") == Fruit::Orange);
+    REQUIRE(Obs::Enum<Fruit>::GetValue("Banana") == Fruit::Banana);
+
+    REQUIRE(Obs::Enum<Fruit>::GetValue(5) == Fruit::Apple);
+    REQUIRE(Obs::Enum<Fruit>::GetValue(6) == Fruit::Orange);
+    REQUIRE(Obs::Enum<Fruit>::GetValue(7) == Fruit::Banana);
+
+    REQUIRE(Obs::Enum<Fruit>::GetUnderlyingValue(Fruit::Apple) == 5);
+    REQUIRE(Obs::Enum<Fruit>::GetUnderlyingValue(Fruit::Orange) == 6);
+    REQUIRE(Obs::Enum<Fruit>::GetUnderlyingValue(Fruit::Banana) == 7);
 }
