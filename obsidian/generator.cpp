f5d64b7e326e27b428cd43603382bb8d7e8d26cb
Marko Kostic <archmarko92@gmail.com>
2026-02-21 17:28:42 +0100

    Add escaping of special characters which might cause correctness issues in attributes or descriptions of elements

--- obsidian/generator.cpp
+++ obsidian/generator.cpp
@@ -42,9 +42,41 @@ static bool WriteToFile(const Opal::StringUtf8& path, const Opal::StringUtf8& co
     {
         return false;
     }
-    fwrite(content.GetData(), 1, content.GetSize(), file);
+    const Opal::u64 written = fwrite(content.GetData(), 1, content.GetSize(), file);
     fclose(file);
-    return true;
+    return written == content.GetSize();
+}
+
+static Opal::StringUtf8 EscapeCppStringLiteral(const Opal::StringUtf8& input)
+{
+    Opal::StringUtf8 result;
+    const char* data = input.GetData();
+    const Opal::u64 size = input.GetSize();
+    for (Opal::u64 i = 0; i < size; i++)
+    {
+        switch (data[i])
+        {
+            case '\\':
+                result += "\\\\";
+                break;
+            case '"':
+                result += "\\\"";
+                break;
+            case '\n':
+                result += "\\n";
+                break;
+            case '\r':
+                result += "\\r";
+                break;
+            case '\t':
+                result += "\\t";
+                break;
+            default:
+                result += Opal::StringUtf8(&data[i], 1);
+                break;
+        }
+    }
+    return result;
 }
 
 static Opal::StringUtf8 QualifiedConstantName(const CppEnum& cpp_enum, const CppEnumConstant& constant)
@@ -69,7 +101,7 @@ static Opal::StringUtf8 GenerateAttributeList(const Opal::DynamicArray<CppAttrib
         {
             result += ", ";
         }
-        result += "{\"" + attributes[i].name + "\", \"" + attributes[i].value + "\"}";
+        result += "{\"" + EscapeCppStringLiteral(attributes[i].name) + "\", \"" + EscapeCppStringLiteral(attributes[i].value) + "\"}";
     }
     return result;
 }
@@ -78,9 +110,9 @@ static Opal::StringUtf8 GenerateEnumSpecialization(const CppEnum& cpp_enum)
 {
     Opal::StringUtf8 result = ObsTemplates::k_enum_template;
 
-    result = ReplaceAll(result, "__enum_full_name__", cpp_enum.full_name);
-    result = ReplaceAll(result, "__enum_name__", cpp_enum.name);
-    result = ReplaceAll(result, "__enum_comment__", cpp_enum.description);
+    result = ReplaceAll(result, "__enum_full_name__", EscapeCppStringLiteral(cpp_enum.full_name));
+    result = ReplaceAll(result, "__enum_name__", EscapeCppStringLiteral(cpp_enum.name));
+    result = ReplaceAll(result, "__enum_comment__", EscapeCppStringLiteral(cpp_enum.description));
 
     // Last entry
     if (!cpp_enum.constants.IsEmpty())
@@ -102,7 +134,7 @@ static Opal::StringUtf8 GenerateEnumSpecialization(const CppEnum& cpp_enum)
             desc_switch += "\n";
         }
         desc_switch += "            case " + QualifiedConstantName(cpp_enum, cpp_enum.constants[i]) + ": return \""
-                       + cpp_enum.constants[i].description + "\";";
+                       + EscapeCppStringLiteral(cpp_enum.constants[i].description) + "\";";
     }
     result = ReplaceAll(result, "__enum_value_to_description_switch__", desc_switch);
 
@@ -115,7 +147,7 @@ static Opal::StringUtf8 GenerateEnumSpecialization(const CppEnum& cpp_enum)
             name_switch += "\n";
         }
         name_switch += "            case " + QualifiedConstantName(cpp_enum, cpp_enum.constants[i]) + ": return \""
-                       + cpp_enum.constants[i].name + "\";";
+                       + EscapeCppStringLiteral(cpp_enum.constants[i].name) + "\";";
     }
     result = ReplaceAll(result, "__enum_value_to_name_switch__", name_switch);
 
@@ -127,7 +159,7 @@ static Opal::StringUtf8 GenerateEnumSpecialization(const CppEnum& cpp_enum)
         {
             name_to_value += "\n";
         }
-        name_to_value += "        if (strcmp(name, \"" + cpp_enum.constants[i].name + "\") == 0) return "
+        name_to_value += "        if (strcmp(name, \"" + EscapeCppStringLiteral(cpp_enum.constants[i].name) + "\") == 0) return "
                          + QualifiedConstantName(cpp_enum, cpp_enum.constants[i]) + ";";
     }
     result = ReplaceAll(result, "__enum_name_to_value_switch__", name_to_value);
@@ -142,10 +174,10 @@ static Opal::StringUtf8 GenerateClassSpecialization(const CppClass& cpp_class)
 {
     Opal::StringUtf8 result = ObsTemplates::k_class_template;
 
-    result = ReplaceAll(result, "__class_scoped_name__", cpp_class.full_name);
-    result = ReplaceAll(result, "__class_name__", cpp_class.name);
-    result = ReplaceAll(result, "__class_scope__", cpp_class.scope);
-    result = ReplaceAll(result, "__class_description__", cpp_class.description);
+    result = ReplaceAll(result, "__class_scoped_name__", EscapeCppStringLiteral(cpp_class.full_name));
+    result = ReplaceAll(result, "__class_name__", EscapeCppStringLiteral(cpp_class.name));
+    result = ReplaceAll(result, "__class_scope__", EscapeCppStringLiteral(cpp_class.scope));
+    result = ReplaceAll(result, "__class_description__", EscapeCppStringLiteral(cpp_class.description));
 
     // Init properties
     Opal::StringUtf8 properties = "{";
@@ -165,8 +197,9 @@ static Opal::StringUtf8 GenerateClassSpecialization(const CppClass& cpp_class)
         Opal::StringUtf8 write_lambda = "[](void* obj, const void* in) { static_cast<" + cpp_class.full_name
                                          + "*>(obj)->" + prop.name + " = *static_cast<const " + member_type + "*>(in); }";
         Opal::StringUtf8 prop_attrs = "{" + GenerateAttributeList(prop.attributes) + "}";
-        properties += "{\"" + prop.name + "\", \"" + prop.description + "\", \"" + prop.type + "\", " + is_pod_str
-                      + ", " + offset_expr + ", " + size_expr + ", " + read_lambda + ", " + write_lambda + ", " + prop_attrs + "}";
+        properties += "{\"" + EscapeCppStringLiteral(prop.name) + "\", \"" + EscapeCppStringLiteral(prop.description) + "\", \""
+                      + EscapeCppStringLiteral(prop.type) + "\", " + is_pod_str + ", " + offset_expr + ", " + size_expr + ", "
+                      + read_lambda + ", " + write_lambda + ", " + prop_attrs + "}";
     }
     properties += "}";
     result = ReplaceAll(result, "__class_init_properties__", properties);
@@ -189,8 +222,8 @@ static Opal::StringUtf8 GenerateEnumCollection(const Opal::DynamicArray<CppEnum>
         {
             entries += ",\n";
         }
-        entries += "        {\"" + cpp_enum.name + "\", \"" + cpp_enum.full_name + "\", \"" + cpp_enum.description + "\", "
-                   + IntToString(cpp_enum.underlying_type_size) + ", {";
+        entries += "        {\"" + EscapeCppStringLiteral(cpp_enum.name) + "\", \"" + EscapeCppStringLiteral(cpp_enum.full_name)
+                   + "\", \"" + EscapeCppStringLiteral(cpp_enum.description) + "\", " + IntToString(cpp_enum.underlying_type_size) + ", {";
 
         for (Opal::u64 j = 0; j < cpp_enum.constants.GetSize(); j++)
         {
@@ -209,7 +242,8 @@ static Opal::StringUtf8 GenerateEnumCollection(const Opal::DynamicArray<CppEnum>
             {
                 value_str = IntToString(constant.value);
             }
-            entries += "{\"" + constant.name + "\", \"" + constant.description + "\", " + value_str + "}";
+            entries += "{\"" + EscapeCppStringLiteral(constant.name) + "\", \"" + EscapeCppStringLiteral(constant.description) + "\", "
+                       + value_str + "}";
         }
         entries += "}, {" + GenerateAttributeList(cpp_enum.attributes) + "}}";
     }
@@ -231,8 +265,8 @@ static Opal::StringUtf8 GenerateClassCollection(const Opal::DynamicArray<CppClas
         {
             entries += ",\n";
         }
-        entries += "        {\"" + cpp_class.name + "\", \"" + cpp_class.scope + "\", \"" + cpp_class.full_name + "\", \""
-                   + cpp_class.description + "\", {";
+        entries += "        {\"" + EscapeCppStringLiteral(cpp_class.name) + "\", \"" + EscapeCppStringLiteral(cpp_class.scope) + "\", \""
+                   + EscapeCppStringLiteral(cpp_class.full_name) + "\", \"" + EscapeCppStringLiteral(cpp_class.description) + "\", {";
 
         for (Opal::u64 j = 0; j < cpp_class.properties.GetSize(); j++)
         {
@@ -250,8 +284,9 @@ static Opal::StringUtf8 GenerateClassCollection(const Opal::DynamicArray<CppClas
             Opal::StringUtf8 write_lambda = "[](void* obj, const void* in) { static_cast<" + cpp_class.full_name
                                              + "*>(obj)->" + prop.name + " = *static_cast<const " + member_type + "*>(in); }";
             Opal::StringUtf8 prop_attrs = "{" + GenerateAttributeList(prop.attributes) + "}";
-            entries += "{\"" + prop.name + "\", \"" + prop.description + "\", \"" + prop.type + "\", " + is_pod_str
-                       + ", " + offset_expr + ", " + size_expr + ", " + read_lambda + ", " + write_lambda + ", " + prop_attrs + "}";
+            entries += "{\"" + EscapeCppStringLiteral(prop.name) + "\", \"" + EscapeCppStringLiteral(prop.description) + "\", \""
+                       + EscapeCppStringLiteral(prop.type) + "\", " + is_pod_str + ", " + offset_expr + ", " + size_expr + ", "
+                       + read_lambda + ", " + write_lambda + ", " + prop_attrs + "}";
         }
         entries += "}, {" + GenerateAttributeList(cpp_class.attributes) + "}}";
     }
@@ -276,7 +311,7 @@ static Opal::StringUtf8 GenerateSingleFile(const CppContext& context)
             {
                 includes += "\n";
             }
-            includes += "#include \"" + filename.GetValue() + "\"";
+            includes += "#include \"" + EscapeCppStringLiteral(filename.GetValue()) + "\"";
         }
     }
     result = ReplaceAll(result, "__refl_includes__", includes);
